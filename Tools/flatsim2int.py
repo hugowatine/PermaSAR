6eeff9d0a37f250c17e69c48fcf6c776cc7997e2
hugowatine <watinehugo@gmail.com>
2025-05-21 16:16:23 +0200

    ajout de flatsim2int.py

---
+++ Tools/flatsim2int.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+############################################
+# Author        : Hugo WATINE (CRPG)
+############################################
+
+"""\
+flatsim2int.py
+-------------
+create .int and .rsc files from flatsim data.
+
+Usage: flatsim2int.py --ifg=<path> --coh=<path>\
+
+Options:
+-h --help           Show this screen.
+--ifg PATH          path of InW .tif file with, in band (1), the wrapped phase
+--coh PATH          path of Coh .tif file with, in band (1), the spatial coherence. Need to be in the same geometry and resolution than InW
+"""
+print()
+print()
+print('Author: Hugo WATINE')
+print()
+print()
+
+import numpy as np
+from osgeo import gdal, osr, gdalconst
+import sys
+import re
+
+gdal.UseExceptions()
+
+try:
+    from nsbas import docopt
+except:
+    import docopt
+
+def open_raster(raster_path):
+    ds = gdal.Open(raster_path, gdalconst.GA_ReadOnly)
+    data = ds.GetRasterBand(1).ReadAsArray()
+    Xsize = ds.RasterXSize
+    Ysize = ds.RasterYSize
+    geotrans = ds.GetGeoTransform()
+    proj = ds.GetProjection()
+
+    return data, Xsize, Ysize, geotrans, proj
+
+def write_rsc(filename, Xsize, Ysize, geotrans, proj):
+    rsc_path = filename + '.rsc'
+    dx = abs(geotrans[1])
+    dy = abs(geotrans[5])
+    ulx = geotrans[0]
+    uly = geotrans[3]
+    
+    with open(rsc_path, 'w') as f:
+        f.write(f"WIDTH {Xsize}\n")
+        f.write(f"FILE_LENGTH {Ysize}\n")
+        f.write(f"X_FIRST {ulx:.6f}\n")
+        f.write(f"Y_FIRST {uly:.6f}\n")
+        f.write(f"X_STEP {dx:.6f}\n")
+        f.write(f"Y_STEP {-dy:.6f}\n")  # Negative if north-up
+        f.write("Z_OFFSET 0.0\n")
+        f.write("Z_SCALE 1.0\n")
+        f.write("PROJECTION UNKNOWN\n")
+
+# read arguments
+arguments = docopt.docopt(__doc__)
+
+ifg_path = arguments["--ifg"]
+coh_path = arguments["--coh"]
+
+phi, Xifg, Yifg, geotrans, proj = open_raster(ifg_path)
+coh, Xcoh, Ycoh, _, _ = open_raster(coh_path)
+
+if Xifg != Xcoh or Yifg != Ycoh:
+    print('ERROR : not the same size')
+    sys.exit
+
+complex_data = coh * np.exp(1j*phi)
+output_path = ifg_path.split('.')[0] + '.int'
+
+drv = gdal.GetDriverByName("roi_pac")
+dst_ds = drv.Create(output_path, Xifg, Yifg, 1, gdal.GDT_CFloat32)
+
+band = dst_ds.GetRasterBand(1)
+band.WriteArray(complex_data.astype(np.complex64))
+dst_ds.SetGeoTransform(geotrans)
+dst_ds.SetProjection(proj)
+
+band.FlushCache()
+del band
+del dst_ds
+
+print(f".int saved: {output_path}")
+
+## Modification of the .rsc file
+
+RLOOK = re.search(r'_(\d+)rlks', output_path).group(1)
+with open(output_path + '.rsc', 'r') as f:
+    lines = f.readlines()
+lines.append(f"{'RLOOK'.ljust(41)}{RLOOK}\n")
+with open(output_path + '.rsc', 'w') as f:
+    f.writelines(lines)
+
+print(f".rsc saved: {output_path+'.rsc'}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
