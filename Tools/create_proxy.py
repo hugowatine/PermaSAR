fe148003e638646424618d23000334badbc6e3de
simondaout <simondaout@gmail.com>
2026-01-30 15:59:23 +0100

    add harcoded NDWI and save output in roi_pac format

--- Tools/create_proxy.py
+++ Tools/create_proxy.py
@@ -11,6 +11,7 @@ import matplotlib.cm as cm
 fichiers_entree = [
     "RMS_geo_8rlks.tiff",
     "NDSI_reprojected.tif",
+    "NDWI_reprojected.tif",
     "inverted_TCoh_geo_8rlks.tiff",
     "CNES_DEM_geo_8rlks_slope_clean.tif"
 ]
@@ -30,25 +31,38 @@ for band in bands_normalized:
     resultat = band * resultat
 
 # normalisation
-resultat = resultat/np.nanpercentile(resultat,98)
+cut = resultat/np.nanpercentile(resultat,98) # higher the value is, higher the cohrence is decreased for unwrapping
+coh = np.clip(1. - cut, 0, 1)   # new weight for filtering
 
-# sauvegarde resultats
+# sauvegarde du cutfile et de la coherence
 dataset_ref = gdal.Open(fichiers_entree[0])
 geotransform = dataset_ref.GetGeoTransform()
 projection = dataset_ref.GetProjection()
-driver = gdal.GetDriverByName("GTiff")
-dataset_sortie = driver.Create("proxy.tif", dataset_ref.RasterXSize, dataset_ref.RasterYSize, 1, gdal.GDT_Float32)
-dataset_sortie.SetGeoTransform(geotransform)
-dataset_sortie.SetProjection(projection)
-band_sortie = dataset_sortie.GetRasterBand(1)
-band_sortie.WriteArray(resultat.astype(np.float32))
+driver = gdal.GetDriverByName("roi_pac")
+
+dataset_cut = driver.Create("proxy.hgt", dataset_ref.RasterXSize, dataset_ref.RasterYSize, 2, gdal.GDT_Float32)
+dataset_cut.SetGeoTransform(geotransform)
+dataset_cut.SetProjection(projection)
+band_cut = dataset_cut.GetRasterBand(1)
+band_cut.WriteArray(cut.astype(np.float32))
+band_cut = dataset_cut.GetRasterBand(2)
+band_cut.WriteArray(cut.astype(np.float32))
+
+dataset_coh = driver.Create("coherence.cor", dataset_ref.RasterXSize, dataset_ref.RasterYSize, 2, gdal.GDT_Float32)
+dataset_coh.SetGeoTransform(geotransform)
+dataset_coh.SetProjection(projection)
+band_coh = dataset_coh.GetRasterBand(1)
+band_coh.WriteArray(coh.astype(np.float32))
+band_coh = dataset_coh.GetRasterBand(2)
+band_coh.WriteArray(coh.astype(np.float32))
 
 # Fermeture des fichiers
-dataset_sortie = None
+dataset_cut = None
+dataset_coh = None
 dataset_ref = None
 
 # Affichage des bandes et du résultat avec matplotlib
-fig, axs = plt.subplots(2, 2, figsize=(12, 8))
+fig, axs = plt.subplots(3, 2, figsize=(12, 8))
 fig.suptitle("Visualisation des bandes et du résultat")
 
 # Affichage des bandes d'entrée
@@ -62,12 +76,18 @@ for i, band in enumerate(bands_normalized):
 
 plt.tight_layout()
 
-fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-fig.suptitle("Proxy")
-# Affichage du résultat
-cax = ax.imshow(resultat, cmap="jet", vmax=0.5, vmin=0)
-ax.axis('off')
-divider = make_axes_locatable(ax)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 6))
+cax = ax1.imshow(cut, cmap="jet", vmax=0.5, vmin=0)
+ax1.set_title('Proxy')
+ax1.axis('off')
+divider = make_axes_locatable(ax1)
+cax_colorbar = divider.append_axes("right", size="5%", pad=0.05)
+plt.colorbar(cax, cax=cax_colorbar)
+
+cax = ax2.imshow(coh, cmap="jet", vmin=0, vmax=1)
+ax2.set_title('Coherence')
+ax2.axis('off')
+divider = make_axes_locatable(ax2)
 cax_colorbar = divider.append_axes("right", size="5%", pad=0.05)
 plt.colorbar(cax, cax=cax_colorbar)
 
